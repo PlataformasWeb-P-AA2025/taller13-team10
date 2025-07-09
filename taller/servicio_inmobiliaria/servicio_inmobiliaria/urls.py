f88fa5c87f12b55f40fa4a6dbaf1b3ed39357107
Carlos <cdmejia4@utpl.edu.ec>
2025-07-09 09:47:16 -0500

    commit para token correccion

--- taller/servicio_inmobiliaria/servicio_inmobiliaria/urls.py
+++ taller/servicio_inmobiliaria/servicio_inmobiliaria/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for servicio_inmobiliaria project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from inmuebles.views import EdificioViewSet, DepartamentoViewSet
+from rest_framework.authtoken import views as drf_views
+
+router = routers.DefaultRouter()
+router.register(r'edificios', EdificioViewSet)
+router.register(r'departamentos', DepartamentoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-token-auth/', drf_views.obtain_auth_token),
 ]
---
+++ taller/token_api.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+try:
+    user_empresa = User.objects.get(username='admin')
+    token, created = Token.objects.get_or_create(user=user_empresa)
+
+    if created:
+        print(f"Token generado para '{user_empresa.username}': {token.key}")
+    else:
+        print(f"Token existente para '{user_empresa.username}': {token.key}")
+
+except User.DoesNotExist:
+    print("El usuario 'empresa_api' no existe. Créalo primero.")
